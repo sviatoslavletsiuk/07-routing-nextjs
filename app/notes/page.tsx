878f0c8e39bdee055bb9585022fe393e95f1ce79
sviatletsiuk-blip <sviatletsiuk@gmail.com>
2026-04-27 08:29:08 +0300

    chore: Remove redundant files
    - Remove app/notes/page.tsx
    - Remove app/notes/Notes.client.tsx

--- app/notes/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function Home() {
-  // Це виправить помилку "Cannot find module" та перенаправить на нотатки
-  redirect("/notes/filter/all");
-}
